3f6391f4296c9cdbbe974fc5fdf8f0636f29a5c0
Sinchana N <sinchana2505@gmail.com>
2025-10-23 19:51:07 +0530

    formatting options page

--- popup.js
+++ popup.js
@@ -1,3 +1,16 @@
 document.getElementById("summarize").addEventListener("click", () => {
-  console.log("Summarize clicked");
+  const result = document.getElementById("result");
+  result.textContent = "Extracting text...";
+
+  chrome.tabs.query({ active: true, currentWindow: true }, ([tab]) => {
+    chrome.tabs.sendMessage(
+      tab.id,
+      { type: "GET_ARTICLE_TEXT" },
+      ({ text }) => {
+        result.textContent = text
+          ? text.slice(0, 300) + "..."
+          : "No Article text found.";
+      }
+    );
+  });
 });
